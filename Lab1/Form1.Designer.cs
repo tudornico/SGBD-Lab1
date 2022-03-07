de6df3cec167846081e938dc52939a34c81e2016
tudornico <tudorelnico@gmail.com>
2022-03-07 12:57:40 +0200

    Add project files.

---
+++ Lab1/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.BuchNummerBox = new System.Windows.Forms.TextBox();
+            this.PriceBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BookNameBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.RegionBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.PagesBox = new System.Windows.Forms.TextBox();
+            this.AuthorBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(278, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 131);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(554, 273);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(603, 131);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 29;
+            this.dataGridView2.Size = new System.Drawing.Size(695, 273);
+            this.dataGridView2.TabIndex = 2;
+            this.dataGridView2.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1152, 506);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 45);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Submit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Insert",
+            "Update",
+            "Delete"});
+            this.comboBox1.Location = new System.Drawing.Point(1119, 48);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 28);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // BuchNummerBox
+            // 
+            this.BuchNummerBox.Location = new System.Drawing.Point(157, 433);
+            this.BuchNummerBox.Name = "BuchNummerBox";
+            this.BuchNummerBox.Size = new System.Drawing.Size(125, 27);
+            this.BuchNummerBox.TabIndex = 5;
+            // 
+            // PriceBox
+            // 
+            this.PriceBox.Location = new System.Drawing.Point(540, 425);
+            this.PriceBox.Name = "PriceBox";
+            this.PriceBox.Size = new System.Drawing.Size(125, 27);
+            this.PriceBox.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 436);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Enter Book Number";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(417, 428);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Enter Book Price";
+            // 
+            // BookNameBox
+            // 
+            this.BookNameBox.Location = new System.Drawing.Point(157, 506);
+            this.BookNameBox.Name = "BookNameBox";
+            this.BookNameBox.Size = new System.Drawing.Size(125, 27);
+            this.BookNameBox.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 509);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Enter Book name";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // RegionBox
+            // 
+            this.RegionBox.Location = new System.Drawing.Point(540, 502);
+            this.RegionBox.Name = "RegionBox";
+            this.RegionBox.Size = new System.Drawing.Size(125, 27);
+            this.RegionBox.TabIndex = 11;
+            this.RegionBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(382, 506);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(152, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Enter Region Number";
+            // 
+            // PagesBox
+            // 
+            this.PagesBox.Location = new System.Drawing.Point(889, 428);
+            this.PagesBox.Name = "PagesBox";
+            this.PagesBox.Size = new System.Drawing.Size(125, 27);
+            this.PagesBox.TabIndex = 13;
+            // 
+            // AuthorBox
+            // 
+            this.AuthorBox.Location = new System.Drawing.Point(889, 503);
+            this.AuthorBox.Name = "AuthorBox";
+            this.AuthorBox.Size = new System.Drawing.Size(125, 27);
+            this.AuthorBox.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(722, 432);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(161, 20);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Enter Number of Pages";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(766, 506);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(111, 20);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Enter Author ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1344, 563);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.AuthorBox);
+            this.Controls.Add(this.PagesBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.RegionBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BookNameBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PriceBox);
+            this.Controls.Add(this.BuchNummerBox);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private DataGridView dataGridView1;
+        private DataGridView dataGridView2;
+        private Button button2 ;
+        private ComboBox comboBox1;
+        private TextBox BuchNummerBox;
+        private TextBox PriceBox;
+        private Label label1;
+        private Label label2;
+        private TextBox BookNameBox;
+        private Label label3;
+        private TextBox RegionBox;
+        private Label label4;
+        private TextBox PagesBox;
+        private TextBox AuthorBox;
+        private Label label5;
+        private Label label6;
+    }
+}
---
+++ Lab1/Form1.cs
@@ -0,0 +1,295 @@
+using System.Data.SqlClient;
+using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Globalization;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            BookNameBox.Hide();
+            PriceBox.Hide();
+            label1.Hide();
+            label2.Hide();
+            label3.Hide();
+            label4.Hide();
+            label5.Hide();
+            label6.Hide();
+            RegionBox.Hide();
+            PagesBox.Hide();
+            AuthorBox.Hide();
+            BuchNummerBox.Hide();
+        }
+
+
+        public void GetData(string command)
+        {
+            String con = "Data Source = DESKTOP-PBGUL9N\\MY_SQL_SERVICE;Initial Catalog=Library;Integrated Security=true";
+
+            
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetData("Select * From Author");
+
+            String con = "Data Source = DESKTOP-PBGUL9N\\MY_SQL_SERVICE;Initial Catalog=Library;Integrated Security=true";
+            String command = "Select * From Author";
+            using (
+                SqlConnection connection = new SqlConnection(con)
+            )
+            {
+                
+                try
+                {
+                    connection.Open();
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(command, con);
+                    SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+
+                    DataTable table = new DataTable
+                    {
+                        Locale = CultureInfo.InvariantCulture
+                    };
+
+                    dataAdapter.Fill(table);
+                    BindingSource binding = new BindingSource();
+                    binding.DataSource = table;
+
+                    dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+                    dataGridView1.DataSource = binding;
+                    connection.Close();
+
+                }
+                catch(Exception ex)
+                {   
+                    Console.Write(ex.Message);
+                    Console.Write("AAAAAAAAAAAA");
+                }
+            }
+           
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs    e)
+        {
+            String con = "Data Source = DESKTOP-PBGUL9N\\MY_SQL_SERVICE;Initial Catalog=Library;Integrated Security=true";
+
+            using (SqlConnection connection = 
+                new SqlConnection(con))
+            {
+                try
+                {
+                    connection.Open();
+                    var AuthorId = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value;
+                   // MessageBox.Show(AuthorId.ToString());
+                    String query = "Select * From Buecher Where AuthorId = ' " + AuthorId.ToString() + "'";
+                    SqlCommand command = new SqlCommand(query);
+
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(query, con);
+                    SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+
+                    DataTable table = new DataTable
+                    {
+                        Locale = CultureInfo.InvariantCulture
+                    };
+
+                    dataAdapter.Fill(table);
+                    BindingSource binding = new BindingSource();
+                    binding.DataSource = table;
+
+                    dataGridView2.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+                    dataGridView2.DataSource = binding;
+                    connection.Close();
+                }
+                catch
+                {
+                    Console.Write("An Error has occured");
+                }
+
+                if(comboBox1.SelectedItem.ToString() == "Insert")
+                {
+                    AuthorBox.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+                }
+            }
+               
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            String command = comboBox1.SelectedItem.ToString();
+            if(command == "Update")
+            {
+                
+                BookNameBox.Text = dataGridView2.Rows[e.RowIndex].Cells[0].Value.ToString();
+                BuchNummerBox.Text = dataGridView2.Rows[e.RowIndex].Cells[1].Value.ToString();
+                RegionBox.Text = dataGridView2.Rows[e.RowIndex].Cells[2].Value.ToString();
+                PagesBox.Text = dataGridView2.Rows[e.RowIndex].Cells[3].Value.ToString();
+                PriceBox.Text = dataGridView2.Rows[e.RowIndex].Cells[4].Value.ToString();
+                AuthorBox.Text = dataGridView2.Rows[e.RowIndex].Cells[5].Value.ToString();
+            }
+
+            if(command == "Delete")
+            {
+                BuchNummerBox.Text = dataGridView2.Rows[e.RowIndex].Cells[1].Value.ToString();
+            }
+            
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            label1.Hide();
+            label2.Hide();
+            label3.Hide();
+            label4.Hide();
+            label5.Hide();
+            label6.Hide();
+            BuchNummerBox.Hide();
+            BuchNummerBox.Clear();
+            PriceBox.Hide();
+            PriceBox.Clear();
+            BookNameBox.Hide();
+            BookNameBox.Clear();
+            RegionBox.Hide();
+            RegionBox.Clear();
+            PagesBox.Hide();
+            PagesBox.Clear();
+            AuthorBox.Hide();
+            String command = comboBox1.SelectedItem.ToString();
+
+
+
+            if(command == "Delete")
+            {
+                label1.Show();
+                BuchNummerBox.Show();
+
+            }
+
+            if(command == "Insert")
+            {
+                label1.Show();
+                label2.Show();
+                label3.Show();
+                label4.Show();
+                label5.Show();
+                label6.Show();
+                BuchNummerBox.Show();
+                PriceBox.Show();
+                BookNameBox.Show();
+                RegionBox.Show();
+                PagesBox.Show();
+                AuthorBox.Show();
+            }
+
+            if(command == "Update")
+            {
+                label2.Show();
+                label3.Show();
+                label4.Show();
+                label5.Show();
+                PriceBox.Show();
+                BookNameBox.Show();
+                RegionBox.Show();
+                PagesBox.Show();
+               
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String command = comboBox1.SelectedItem.ToString();
+            String con = "Data Source = DESKTOP-PBGUL9N\\MY_SQL_SERVICE;Initial Catalog=Library;Integrated Security=true";
+            if (command == "Delete")
+            {
+                String Query = "Delete From Buecher Where Buchnummer = '" + BuchNummerBox.Text + "'";
+                using (SqlConnection connection = 
+                    new SqlConnection(con))
+                {
+                    try
+                    {
+                        connection.Open();
+                        SqlCommand cmd = new SqlCommand(Query, connection);
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        connection.Close();
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Sorry there isn't a book with that ID");
+                    }
+                }
+            }
+
+            if(command == "Insert")
+            {
+                String Query = "Insert into Buecher Values ('" + BookNameBox.Text + "' , '" + BuchNummerBox.Text + "' , '" +
+                    RegionBox.Text + "' , '" + PagesBox.Text + "' , '" + PriceBox.Text + "' , '" + AuthorBox.Text + "')";
+                using (SqlConnection connection =
+                    new SqlConnection(con))
+                {
+                    try
+                    {
+                        connection.Open();
+                        SqlCommand cmd = new SqlCommand(Query, connection);
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        connection.Close();
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Sorry a problem has occured !!");
+                    }
+                }
+            }
+
+            if(command == "Update")
+            {
+                String Query = "Update Buecher " +
+                    "set BookName = '" + BookNameBox.Text + "' , " +
+                    "set Region_nummer = " + RegionBox.Text + " , " +
+                    "set Number_of_pages = " + PagesBox.Text + " , " +
+                    "set Preis = " + PriceBox.Text +
+                    " Where AuthorId = " + AuthorBox.Text +";" ;
+                MessageBox.Show(Query);
+                using(SqlConnection connection = 
+                    new SqlConnection(con))
+                {
+                    try
+                    {
+                        connection.Open();
+                        SqlCommand cmd = new SqlCommand(Query, connection);
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        connection.Close();
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Sorry an error has occured !");
+                    }
+                }
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
